9c662afd595a617a973de128a2d2ddb18312a109
Sebastian_Suarez_G <sesuga7243@gmail.com>
2020-11-04 19:00:36 -0500

    se crean 11 clientes

---
+++ clientes.sql
@@ -0,0 +1,34 @@
+-- Se crean 11 clientes
+insert into cliente
+values ('100','Tony','Stark','iron.man@avengers.com');
+
+insert into cliente
+values ('200','Steve','Rogers','captain.america@avengers.com');
+
+insert into cliente
+values ('300','Thor','Odinson','thor@avengers.com');
+
+insert into cliente
+values ('400','Natasha','Romanoff','black.widow@avengers.com');
+
+insert into cliente
+values ('500','Bruce','Banner','hulk@avengers.com');
+
+insert into cliente
+values ('600','Peter','Parker','spider.man@avengers.com');
+
+insert into cliente
+values ('700','Stephen','Strange','doctor.strange@avengers.com');
+
+insert into cliente
+values ('800','Wanda','Maximoff','scarlet.witch@avengers.com');
+
+insert into cliente
+values ('900','Carol','Danvers','captain.marvel@avengers.com');
+
+insert into cliente
+values ('1000','Sam','Wilson','falcon@avengers.com');
+
+insert into cliente
+values ('1100','Scott','Lang','ant.man@avengers.com');
+
