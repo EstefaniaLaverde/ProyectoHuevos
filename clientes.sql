80b98cb1cefc597043cab90493d0e19a553706c4
Sebastian_Suarez_G <sesuga7243@gmail.com>
2020-11-04 19:51:18 -0500

    se crean 11 clientes y sus telefonos

--- clientes.sql
+++ clientes.sql
@@ -32,3 +32,37 @@ values ('1000','Sam','Wilson','falcon@avengers.com');
 insert into cliente
 values ('1100','Scott','Lang','ant.man@avengers.com');
 
+-- Se crean los respectivos telefonos
+
+insert into telefono (id_telefono,num_telefono,id_cliente_cliente)
+values ('1','1111111','100');
+
+insert into telefono (id_telefono,num_telefono,id_cliente_cliente)
+values ('2','2222222','200');
+
+insert into telefono (id_telefono,num_telefono,id_cliente_cliente)
+values ('3','3333333','300');
+
+insert into telefono (id_telefono,num_telefono,id_cliente_cliente)
+values ('4','4444444','400');
+
+insert into telefono (id_telefono,num_telefono,id_cliente_cliente)
+values ('5','5555555','500');
+
+insert into telefono (id_telefono,num_telefono,id_cliente_cliente)
+values ('6','6666666','600');
+
+insert into telefono (id_telefono,num_telefono,id_cliente_cliente)
+values ('7','7777777','700');
+
+insert into telefono (id_telefono,num_telefono,id_cliente_cliente)
+values ('8','8888888','800');
+
+insert into telefono (id_telefono,num_telefono,id_cliente_cliente)
+values ('9','9999999','900');
+
+insert into telefono (id_telefono,num_telefono,id_cliente_cliente)
+values ('10','1010101','1000');
+
+insert into telefono (id_telefono,num_telefono,id_cliente_cliente)
+values ('11','1110111','1100');
