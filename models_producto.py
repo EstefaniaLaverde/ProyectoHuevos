66383417b4b3a3b1167b09a7fca9fe886d6f102e
sebastianssg <56741758+sebastianssg@users.noreply.github.com>
2020-11-08 12:19:25 -0500

    correcciones

--- models_producto.py
+++ models_producto.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+from django.contrib import admin
+
 # Create your models here.
 
 class categoria(models.Model):
     id_categoria = models.IntegerField(primary_key=True)
     tipo = models.CharField(max_length=45)
-    tamanio = models.SamllIntegerField()
+    tamanio = models.SmallIntegerField()
 
 class administrador(models.Model):
     id_administrador = models.AutoField(primary_key=True)
@@ -17,7 +19,11 @@ class producto(models.Model):
     id_producto = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=45)
     precio = models.IntegerField()
-    stock = models.SamllIntegerField()
+    stock = models.SmallIntegerField()
     cantidad_venta = models.IntegerField()
     id_categoria = models.ForeignKey(categoria, on_delete=models.CASCADE)
     id_administrador = models.ForeignKey(administrador, on_delete=models.CASCADE)
+
+admin.site.register(categoria)
+admin.site.register(administrador)
+admin.site.register(producto)
