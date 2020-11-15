b3ad25f2b1ec124e500a742bc1c54715ffadf95c
Estefania Laverde <estefania.laverde@urosario.edu.co>
2020-11-14 23:50:03 -0500

    Servicios de update y delete en repartidores

--- ProyectoHuevos/apps/factura/urls.py
+++ ProyectoHuevos/apps/factura/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from apps.factura.views import index, crearRepartidor, consultarRepartidor
+from apps.factura.views import index, crearRepartidor, consultarRepartidor, editarRepartidor, eliminarRepartidor
 
 urlpatterns = [
     path('', index),
     path('crearRepartidor/', crearRepartidor, name = 'crearRepartidor'),
     path('consultarRepartidor', consultarRepartidor, name = 'consultarRepartidor'),
+    path('editarRepartidor/<id_rep>', editarRepartidor, name = 'editarRepartidor'),
+    path('eliminarRepartidor/<id_rep>', eliminarRepartidor, name = 'eliminarRepartidor'),
+
 ]
--- ProyectoHuevos/apps/factura/views.py
+++ ProyectoHuevos/apps/factura/views.py
@@ -12,12 +12,32 @@ def crearRepartidor(request):
     if request.method == 'POST':
         form = RepartidorForm(request.POST)
         form.save()
+        return consultarRepartidor(request)
     else:
         form = RepartidorForm()
-    return render(request, 'repartidor/crearRepartidor.html', {'form' : form})
+        return render(request, 'repartidor/crearRepartidor.html', {'form' : form})
 
 def consultarRepartidor(request):
     repartidores = repartidor.objects.all()
     contexto = {'repartidores':repartidores}
 
     return render(request, 'repartidor/consultarRepartidor.html', contexto)
+
+def editarRepartidor(request, id_rep):
+    Repartidor = repartidor.objects.get(id_repartidor = id_rep)
+    if request.method == 'POST':
+        form = RepartidorForm(request.POST, instance = Repartidor)
+        if form.is_valid:
+            form.save()
+        return consultarRepartidor(request)
+    else:
+        form = RepartidorForm(instance = Repartidor)
+        return render(request, 'repartidor/editarRepartidor.html', {'form' : form})
+
+def eliminarRepartidor(request, id_rep):
+    Repartidor = repartidor.objects.get(id_repartidor=id_rep)
+    if request.method == 'POST':
+        Repartidor.delete()
+        return consultarRepartidor(request)
+    else:
+        return render(request, 'repartidor/eliminarRepartidor.html', {'repartidor' : Repartidor})
